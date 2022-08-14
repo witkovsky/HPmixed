6976fa1e4bce4a29e721eb93b678600e8ed6ffe6
Viktor <witkovsky@gmail.com>
2022-08-14 23:07:43 +0200

    Update getAnova.m

--- getAnova.m
+++ getAnova.m
@@ -14,7 +14,7 @@ function result = getAnova(lmefit,options)
 %         options.STAT.Description = ''
 %         options.STAT.colnames = 'fit_'
 %         options.STAT.effectID = []
-%         options.STAT.ddfMethod = 'Satterthwaite'
+%         options.STAT.ddfMethod = 'Fai-Cornelius' ('ChiSquare')
 %
 % EXAMPLE 0: (Fit and analysis of the SplitPlotData by using HPMIXED)
 %   clear
@@ -22,8 +22,13 @@ function result = getAnova(lmefit,options)
 %   formula  = 'y ~ A + B + A:B + (1 | Block) + (1 | Block:A)';
 %   model = hpmixedmodel(SplitPlotData,formula);
 %   lmefit = hpmixed(model);
+%   options.STAT.ddfMethod = 'Fai-Cornelius';
+%   anovaF  = getAnova(lmefit,options)
+%   options.STAT.ddfMethod = 'ChiSquare';
+%   anovaChi = getAnova(lmefit,options)
 
 % (c) Viktor Witkovsky (witkovsky@savba.sk)
+% Revision: 14-Aug-2022 16:44:34
 % Ver.: 16-Mar-2014 18:00:15
 
 %% CHECK INPUTS / OUTPUTS
@@ -51,7 +56,7 @@ P       = zeros(nTerms,1);
 F       = zeros(nTerms,1);
 DF1     = zeros(nTerms,1);
 DF2     = zeros(nTerms,1);
-Lambda = cell(1,nTerms);
+Lambda  = cell(1,nTerms);
 
 for k = 1:nTerms
     Cols        = find(XCols2Terms == k);
@@ -59,17 +64,23 @@ for k = 1:nTerms
     Lambda{k}   = sparse(q + Cols,1:nCols,1,m,nCols);
     rowTab      = getStats(Lambda{k},lmefit,options);
     P(k)        = rowTab.TABLE.pValue;
-    F(k)        = rowTab.TABLE.Fstat;
+    F(k)        = rowTab.TABLE.Stat;
     DF1(k)      = rowTab.TABLE.NDF;
     DF2(k)      = rowTab.TABLE.DDF;
 end
 
-result.TABLE = dataset({TermNames,'Term'},{F,'FStat'},{DF1,'DF1'},...
-    {DF2,'DF2'},{P,'pValue'});
+switch lower(options.STAT.ddfMethod)
+    case {'faicornelius' 'fc' 'fai' 'fai-cornelius' 'cornelius'}
+        result.TABLE = dataset({TermNames,'Term'},{F,'FStat'},...
+            {DF1,'DF1'},{DF2,'DF2'},{P,'pValue'});
+    case {'chisquare' 'chi' 'chi-square'}
+        result.TABLE = dataset({TermNames,'Term'},{F,'ChiSquareStat'},...
+            {DF1,'DF1'},{DF2,'DF2'},{P,'pValue'});
+end
+
 result.Details.Lambda = Lambda;
 result.options = options;
 
 disp(result.TABLE)
 
 end     % END of getAnova
-
